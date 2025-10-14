b2c57bb4da88ba762e7229cec78367c580ec0bba
kimdanha <lily034686@gmail.com>
2025-10-15 01:52:43 +0900

    feat: Calculator 구현

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,4 +1,14 @@
 package calculator.model;
 
+import java.util.List;
+
 public class Calculator {
+
+    public static int sum(List<Integer> numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            result += number;
+        }
+        return result;
+    }
 }
