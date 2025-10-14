994e35f2c3f8b07e85e9d6a4fe337446b24dd3f6
kimdanha <lily034686@gmail.com>
2025-10-15 01:54:31 +0900

    feat: 컨트롤러 통합

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,24 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
+import calculator.model.Parser;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class CalculatorController {
+
+    public void run() {
+        try {
+            String input = InputView.readInput();
+            List<Integer> numbers = Parser.parse(input);
+            int result = Calculator.sum(numbers);
+            OutputView.printResult(result);
+        } catch (NoSuchElementException e) {
+            OutputView.printResult(0);
+        }
+    }
 }
+
